136e35a6e8848ab93690b7e8dc5d5028df2ead4a
Martin Jenigár <martin.jenigar@student.tuke.sk>
2021-10-06 01:05:56 +0200

    empty world with one little boid

--- static/js/boid.js
+++ static/js/boid.js
@@ -1,21 +1,22 @@
-class Boid {
-    constructor() {
-        this.position = createVector(width/2, height/2)
-        this.velocity = p5.Vector.random2D();
-        this.acceleration = createVector();
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+
+export default class Boid {
+        constructor(world) {
+            // this.position = [x, y, z]
+            // this.velocity = p5.Vector.random2D();
+            // this.acceleration = createVector();
+            
+            this.coneGeo = new THREE.ConeGeometry(2, 2),
+            this.coneMaterial = new THREE.MeshStandardMaterial({
+                color: 0x00ff00
+            }),
+            this.boid = new THREE.Mesh(this.coneGeo, this.coneMaterial);
+
+            world.scene.add(this.boid);
+        }
+
+        // update() {
+        //     this.position.add(this.velocity);
+        //     this.velocity.add(this.acceleration);
+        // }
     }
-
-
-    update() {
-        this.position.add(this.velocity);
-        this.velocity.add(this.acceleration);
-    }
-
-    show() {
-
-        // radius, height, 
-        cone(15, 30, 30, 30, true);
-    }
-    
-
-}
