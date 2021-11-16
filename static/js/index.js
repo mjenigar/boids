1c0b7acf9f2199c5a81e39340fb147c55e80c5d5
Martin Jenigár <martin.jenigar@student.tuke.sk>
2021-11-16 20:36:23 +0100

    index page

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("Aaa");
+
+        VANTA.BIRDS({
+            el: "#header",
+            mouseControls: false,
+            touchControls: false,
+            gyroControls: false,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            scale: 1.00,
+            scaleMobile: 1.00,
+            backgroundColor: 0xb0f1e,
+            color1: 0xd42ff,
+            color2: 0x9301ff,
+            colorMode: "lerpGradient",
+            quantity: 3.00
+        })
+    })
